77b265e8b4768641aa4f84937a4acc63f2c01542
JulesAsh <julia.ts94@gmail.com>
2019-11-15 23:54:18 +0500

    jquery for before-after slide and catalogue cards

---
+++ js/before-slider.js
@@ -0,0 +1,14 @@
+$(window).load(function() {
+    $(".before-after").twentytwenty({
+        before_label:'Без скинали', // Set a custom before label
+        after_label:'Со скинали' // Set a custom after label
+    });
+    $('.before__slider').slick({
+        draggable: false,
+        dots: true,
+        dotsClass: 'before__slider-dots',
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right')
+    });
+
+});
