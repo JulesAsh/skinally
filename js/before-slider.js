dc553535d214b59cf29168dd47a15dee97f02887
JulesAsh <julia.ts94@gmail.com>
2019-11-18 01:04:30 +0500

    fixed forms, optimized css amd a map

--- js/before-slider.js
+++ js/before-slider.js
@@ -13,5 +13,16 @@ $(window).load(function() {
     $('.menu-button').on('click', function(){
         $('.menu').toggleClass('menu__active');
     });
+    // показывает карту
+    var reviews= $('.reviews');
+    var reviewsTop = reviews.offset().top;
+    $(window).bind('scroll', function(){
+        var windowTop = $(this).scrollTop();
+        if (windowTop > reviewsTop) {
+            $('#map').html('<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A6a3800b4d9f1cd0405a0fb143e2aeeddc662b2fff91310b04536011baf4344bc&amp;width=100%25&amp;height=410&amp;lang=ru_RU&amp;scroll=false"></script>'
+            )
+            $(window).unbind('scroll');
+        }
+    })
 
 });
